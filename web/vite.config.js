7fe4bb7eacbf41bfe1ae2edd6a2a2b8d8e6d65a5
NightKnight-nx2 <musabugrademir@gmail.com>
2026-04-08 19:41:50 +0300

    Remove web app folder (using desktop app instead)

--- web/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './', // For GitHub Pages relative paths
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-});
